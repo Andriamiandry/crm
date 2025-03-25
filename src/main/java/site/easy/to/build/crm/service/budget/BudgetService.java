3edef8986ac3e7cae0e8539d6130e20f2eb3d43c
Andriamiandry <andriamiandryherivony@gmail.com>
2025-03-25 23:16:26 +0300

    Budget conclude

--- src/main/java/site/easy/to/build/crm/service/budget/BudgetService.java
+++ src/main/java/site/easy/to/build/crm/service/budget/BudgetService.java
@@ -1,21 +1,9 @@
 package site.easy.to.build.crm.service.budget;
 
 import java.math.BigDecimal;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import java.math.RoundingMode;
 import java.util.List;
 import java.util.Optional;
-=======
-import java.util.List;
->>>>>>> Stashed changes
-=======
-import java.util.List;
->>>>>>> Stashed changes
-=======
-import java.util.List;
->>>>>>> Stashed changes
 
 import org.springframework.stereotype.Service;
 
@@ -26,23 +14,8 @@ import site.easy.to.build.crm.repository.BudgetRepository;
 public class BudgetService {
     private final BudgetRepository budgetRepository;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     public BudgetService(BudgetRepository budgetRepository) {
         this.budgetRepository = budgetRepository;
-=======
-    public BudgetService() {
-        this.budgetRepository = null;
->>>>>>> Stashed changes
-=======
-    public BudgetService() {
-        this.budgetRepository = null;
->>>>>>> Stashed changes
-=======
-    public BudgetService() {
-        this.budgetRepository = null;
->>>>>>> Stashed changes
     }
 
     public Budget save(Budget budget) {
@@ -62,24 +35,5 @@ public class BudgetService {
         return totalBudget;
     }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-    // public BigDecimal getRealBudget(int customerId) {
-    //     return getTotalCustomerBudgets(customerId).subtract(expenseService.getCustomerDepense(customerId));
-    // }
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+    
 }
