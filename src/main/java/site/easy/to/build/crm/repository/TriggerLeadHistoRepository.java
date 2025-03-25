0543ad9624a6cc2357dfb207851ad095a0b067e0
Andriamiandry <andriamiandryherivony@gmail.com>
2025-03-25 23:54:54 +0300

    Notification
    Well done

--- src/main/java/site/easy/to/build/crm/repository/TriggerLeadHistoRepository.java
+++ src/main/java/site/easy/to/build/crm/repository/TriggerLeadHistoRepository.java
@@ -1,5 +1,7 @@
 package site.easy.to.build.crm.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import site.easy.to.build.crm.entity.TriggerLeadHisto;
 
 @Repository
 public interface TriggerLeadHistoRepository extends JpaRepository<TriggerLeadHisto, Long> {
+    List<TriggerLeadHisto> findByCustomerCustomerId(int idCustomer);
 }
--- src/main/java/site/easy/to/build/crm/service/budget/BudgetService.java
+++ src/main/java/site/easy/to/build/crm/service/budget/BudgetService.java
@@ -8,14 +8,22 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import site.easy.to.build.crm.entity.Budget;
+import site.easy.to.build.crm.entity.RateConfig;
 import site.easy.to.build.crm.repository.BudgetRepository;
+import site.easy.to.build.crm.service.rate.RateConfigService;
+import site.easy.to.build.crm.service.expense.ExpenseService;
 
 @Service
 public class BudgetService {
     private final BudgetRepository budgetRepository;
+    private final ExpenseService expenseService;
+    private final RateConfigService rateConfigService;
 
-    public BudgetService(BudgetRepository budgetRepository) {
+    public BudgetService(BudgetRepository budgetRepository, ExpenseService expenseService,
+            RateConfigService rateConfigService) {
         this.budgetRepository = budgetRepository;
+        this.expenseService = expenseService;
+        this.rateConfigService = rateConfigService;
     }
 
     public Budget save(Budget budget) {
@@ -26,6 +34,7 @@ public class BudgetService {
     public List<Budget> getCustomerBudgets(int customerId) {
         return budgetRepository.findByCustomerCustomerId(customerId);
     }
+
     public BigDecimal getTotalCustomerBudgets(int customerId) {
         List<Budget> budgets = getCustomerBudgets(customerId);
         BigDecimal totalBudget = BigDecimal.ZERO;
@@ -35,5 +44,33 @@ public class BudgetService {
         return totalBudget;
     }
 
-    
+    public BigDecimal getRealBudget(int customerId) {
+        return getTotalCustomerBudgets(customerId).subtract(expenseService.getCustomerDepense(customerId));
+    }
+
+    public boolean isBudgetTargetReached(int customerId, BigDecimal add) {
+        return (expenseService.getCustomerDepense(customerId).add(add))
+                .compareTo(getTotalCustomerBudgets(customerId)) > 0;
+    }
+
+    public boolean isRateAlertReached(int customerId, BigDecimal add) {
+        Optional<RateConfig> rateConfig = rateConfigService.findLatest();
+        BigDecimal tauxAlert = rateConfig.get().getRate();
+
+        BigDecimal totalBudgets = getTotalCustomerBudgets(customerId);
+        BigDecimal totalExpenses = expenseService.getCustomerDepense(customerId).add(add);
+
+        if (totalBudgets.compareTo(BigDecimal.ZERO) == 0) {
+            return false;
+        }
+
+        BigDecimal pourcentageDepense = totalExpenses
+                .multiply(new BigDecimal("100"))
+                .divide(totalBudgets, 2, RoundingMode.HALF_UP);
+        System.out.println("budget: " + totalBudgets + "");
+        System.out.println("expense: " + totalExpenses + "");
+        System.out.println("tauxAlert: " + tauxAlert + "");
+
+        return pourcentageDepense.compareTo(tauxAlert) >= 0;
+    }
 }
